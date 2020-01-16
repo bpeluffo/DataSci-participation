b1c7de4b42381e20e6efc19a2fc352617f3e59f3
bpeluffo <59966525+bpeluffo@users.noreply.github.com>
2020-01-16 11:49:12 -0500

    class meeting 1

---
+++ class meeting 1.R
@@ -0,0 +1,38 @@
+
+
+number<-3
+number
+number*2
+number<-5
+number*2
+number
+c(1,2,3,4)
+times<- c(17,30,25,35,25,30,40,20)
+times
+mean(times)
+times/60
+time_hours<-times/60
+range(times)
+sqrt(times)
+times>30
+times==17
+which(times>30)
+help()
+?any
+?mean
+times[times>30]
+times[3]
+times[-3](everything
+times[3:5]
+3:5
+times[c(2, 4)]
+times[times>30]
+times[1]<-47
+times
+times[times>30]<-NA
+times
+mean(times)
+mean(times,na.rm = TRUE)
+mtcars
+str(mtcars)
+names(mtcars)
